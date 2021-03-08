8e0a3f15de8c1a65e94b3c20e2aa9a11e5a97c53
healthyred <jhl3@williams.edu>
2021-03-07 23:32:47 -0500

    ethereum cli

--- coingecko.py
+++ coingecko.py
@@ -3,13 +3,18 @@ import json
 import requests
 import click
 
-# def get_price():
-#     resp = requests.get(
-#         'https://api.coingecko.com/api/v3/simple/price',
-#         params={'ids': 'ethereum', 'vs_currencies': 'usd'}
-#     )
-#     return resp.json()['ethereum']['usd']
+def get_price(coinName, vsCurrency):
+    resp = requests.get(
+        'https://api.coingecko.com/api/v3/simple/price',
+        params={'ids': coinName, 'vs_currencies': vsCurrency}
+    )
+    return resp.json()['ethereum']['usd']
 
 @click.command()
-def cli():
-    click.echo("Hello World!")
+@click.option('--coin_name', default='ethereum',
+              help='This is the cryptocurrency we will query for.')
+@click.option('--vs_currency', default='usd',
+              help='This is the currency we will show value in.')
+def cli(coinName, vsCurrency):
+    click.echo(get_price(coinName, vsCurrency))
+
--- setup.py
+++ setup.py
@@ -13,16 +13,3 @@ setup(
         coingecko=coingecko:cli
     ''',
 )
-
-# setup(
-#     name='HelloWorld',
-#     version='1.0',
-#     py_modules=['hello'],
-#     install_requires=[
-#         'Click',
-#     ],
-#     entry_points='''
-#         [console_scripts]
-#         hello=hello:cli
-#     ''',
-# )
