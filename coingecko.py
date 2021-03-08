cf1c942e807d2cb39b92f801af37c59fd368a7ce
healthyred <jhl3@williams.edu>
2021-03-07 23:35:14 -0500

    fixed naming

--- coingecko.py
+++ coingecko.py
@@ -11,10 +11,10 @@ def get_price(coinName, vsCurrency):
     return resp.json()['ethereum']['usd']
 
 @click.command()
-@click.option('--coin_name', default='ethereum',
+@click.option('--coin-name', default='ethereum',
               help='This is the cryptocurrency we will query for.')
-@click.option('--vs_currency', default='usd',
+@click.option('--vs-currency', default='usd',
               help='This is the currency we will show value in.')
-def cli(coinName, vsCurrency):
+def cli(coin_name, vs_currency):
     click.echo(get_price(coinName, vsCurrency))
 
